37cdc205a4d7dd5e5b72d574d70c9cf8f0259f57
unknown <erica.gunn@gmail.com>
2015-12-11 22:33:35 -0500

    new version, more complicated, but more like dataset. Also, comments.

--- script/script.js
+++ script/script.js
@@ -1,3 +1,7 @@
+//The first version that I committed was pretty simple; this one might come a little closer to actually
+//mimicking your dataset (and it has comments!)
+
+
 /*Start by setting up the canvas */
 var margin = {t:50,r:100,b:50,l:50};
 var width = document.getElementById('plot').clientWidth - margin.r - margin.l,
@@ -44,51 +48,126 @@ var lineGenerator = d3.svg.line()
     .y(function(d){ return scaleY(d.value)})
     .interpolate('basis');
 
-zerosArray = [];
+queue()
+    .defer(d3.csv,'data/fakeCountryData.csv',parseCountry)
+    .defer(d3.csv,'data/yearMetadata.csv',parseYear)
+    .await(dataLoaded);
 
-for (i=0; i<20;i++){
-    zerosArray.push({year:2000+i,value:10});
-}
+function dataLoaded(err,countryDataIn,metadataIn){
 
-//draw(zerosArray);
+    //This probably shouldn't all be in the dataLoaded function, but it works here, so I'm not going to be
+    // fancy and try to organize it
 
-//function draw(dataArray) { //runs each time the button is clicked! Parameter passes button ID - sends right dataArray depnding on input
-    //can't use append in here - will add new elements each time a button is clicked!
-    //only want to update the path in the draw function.
+    //check that the data is here, see what it looks like
+    //console.log(countryDataIn);
 
+    //nest the data to sort by country
+    var nestedCountries = d3.nest()
+        .key(function (d) {
+            return d.country
+        })
+        .entries(countryDataIn);
 
+    //double check that it does it
+    console.log(nestedCountries);
 
-    console.log(zerosArray);
+    //create an array of zeros as a placeholder; declare null array, then populate with a for loop
+    //(Note that I'm populating the array with objects, which have two attributes: year, and value
+    //this way, I can access them with zerosArray.year and .value)
+    var zerosArray = [];
+    for (i=0; i<20;i++){
+        zerosArray.push({year:2000+i,value:10});
+    }
 
-    plot.append('path')
+    //create an array of these zeros arrays, one for each country in the data set (three in my case)
+    //could also do this with a for loop, for bigger datasets
+    emptyData = [zerosArray, zerosArray, zerosArray];
+
+    //declare and populate a lookup table to index year to a particular position in the emptyData arrays
+    yearMetadata = d3.map();
+    for (i = 0; i < metadataIn.length; i++) {
+        yearMetadata.set(metadataIn[i].year, metadataIn[i].index);
+    }
+
+    //create a DOM group for each country in emptyData (should be three - one for each country)
+    //Will append lines into these groups later - for now, just want one for each element in emptyData
+    var countryLines = plot.selectAll('country-groups')
+        .data(emptyData)
+        .enter()
+        .append('g')
+        .attr('class','country-groups')
+        //translate each group to create the stacked graph look
+        .attr('transform', function(d,i){return 'translate('+10*i+ ',' +10*i+ ')'});
+
+    //append a path to each country group (should be 3 of them with this one append, since you are appending one to
+    //each group created above)
+    countryLines
+        .append('path')
         .attr('class', 'data-line');
 
-    //update the path item defined above, using the new params passed in
-    plot.select('.data-line')
-        .datum(zerosArray)
-        //.transition()
-        //.duration(500)
-        .attr('d', lineGenerator)
+    //console.log(emptyData[1]);
+
+    //bind the empty data array to populate the path items with zeros (should give 3 flat lines)
+    var eachLine = plot.selectAll('.data-line')
+        .data(emptyData)
+        //need to use the accessor function here, because you want to do this for each item in emptyData;
+        //this binds one element of emptyData to each group created above, making 3 lines
+        .attr('d', function(d,i){return lineGenerator(emptyData[i])})
         .style('stroke','gray');
 
 
-//listen for user click event
-d3.selectAll('.btn').on('click',function(){
-    var type = d3.select(this).attr('id');  //id values are coffee and tea (from HTML)
+    //listen for user click event
+    d3.selectAll('.btn').on('click', function () {
+        var type = d3.select(this).attr('id');  //id values are coffee and tea (from HTML)
 
-    dataArray = zerosArray;
+        //console.log("I heard you");
 
-    dataArray[10].value = 50;
-
-    console.log(dataArray);
-
-
-//update the path item defined above, using the new params passed in
-    plot.select('.data-line')
-        .datum(dataArray)
-        .transition()
-        .duration(500)
-        .attr('d', lineGenerator);
-});
+        //take the first "year" value in nestedCountries[0].values[1] (Japan), and use metaData to find what its index
+        //should be in the zeros array for Japan. Print it to the console to check that it's the right one
+        //console.log(yearMetadata.get([nestedCountries[0].values[1].year]));
 
 
+// *****need to put this inside a for loop to update for each country - right now, only grabs information from
+        //one entry in the nestedCountries array
+
+        //store index temporarily
+        temp = yearMetadata.get([nestedCountries[0].values[1].year]);
+
+        //check that you can use the temp variable to access the emptyData array
+        //console.log((emptyData[0])[temp].value);
+
+        //create a copy of emptyData to update with values extracted from nestedCountries
+        updatedData = emptyData;
+
+        //overwrite the appropriate value for the updatedData array with the value stored in the temp variable
+        //(in this case, (emptyData[0])[13].value)
+        (updatedData[0])[temp].value = 50;
+
+        //check that it updated
+        console.log((updatedData[0])[temp].value);
+
+
+        //update the path based on the updatedData array
+        plot.select('.data-line')
+            .datum(updatedData)
+            .transition()
+            .duration(500)
+            .attr('d', function(d,i){return lineGenerator(updatedData[i])});
+    });
+
+}
+
+
+function parseCountry(d){
+    return {
+        country: d.country,
+        year: +d.year
+    }
+}
+
+
+function parseYear(d){
+    return {
+        year: d.year,
+        index: d.index    }
+}
